050d7f6be1636f3cf63edcd962ff816428534910
Frank <frankz.lu@gmail.com>
2019-10-20 17:35:52 +0200

    proto done

---
+++ tools/keysim.py
@@ -0,0 +1,23 @@
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
+
+class Sim:
+    
+    def __init__(self):
+        self.m = PyMouse()
+        self.k = PyKeyboard()
+    def click(self):
+        x_dim, y_dim = self.m.screen_size()
+        self.m.click(int(x_dim/2), int(y_dim/2), 1)
+
+    def pressLeft(self):
+        self.tap(self.k.left_key)
+        return "left"
+
+    def pressRight(self):
+        self.tap(self.k.right_key)
+        return "right"
+
+    def tap(self, key):
+        self.k.tap_key(key)
+        return key
